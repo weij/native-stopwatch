32ba1d095de47261ca4b2fa29efb16f680c7296c
Weijun Yin <gw9812@gmail.com>
2016-05-18 14:58:40 +0800

    handle startStopButton onPress event with setInterval

--- index.ios.js
+++ index.ios.js
@@ -1,16 +1,24 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet, AppRegistry } from 'react-native';
+import { View, Text, TouchableHighlight, StyleSheet, AppRegistry } from 'react-native';
 
 class StopWatch extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      timeElapsed: null
+    }
+  }
+
   render(){
     return <View style={style.container}>
       <View style={[style.header, this._border('yellow')]}>  
-        <View style={this._border('red')}>
+        <View style={[style.timerWrapper, this._border('red')]}>
           <Text>
-            00:00.00
+            {this.state.timeElapsed}
           </Text>
         </View>
-        <View style={this._border('green')}>  
+        <View style={[style.buttonWrapper, this._border('green')]}>  
           {this._renderStartStopButton()}
           {this._renderLapButton()}
         </View>
@@ -23,12 +31,25 @@ class StopWatch extends Component {
     </View>
   }
 
+  _onButtonPress(){
+    const startTime = new Date();
+
+    setInterval(() => {
+      this.setState({ timeElapsed: new Date() - startTime});
+    }, 30);
+
+  }
+
   _renderStartStopButton(){
-    return <View>
+
+    return <TouchableHighlight
+      underlayColor='gray'
+      onPress={this._onButtonPress.bind(this)}
+      >
       <Text>
         Start
       </Text>
-    </View>
+    </TouchableHighlight>
   }
 
   _renderLapButton(){
@@ -57,6 +78,17 @@ const style = StyleSheet.create({
   },
   footer: {
     flex: 1
+  },
+  timerWrapper: {
+    flex: 5,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  buttonWrapper: {
+    flex: 3,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center'
   }
 });
 
